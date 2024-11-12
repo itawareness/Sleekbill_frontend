1ec6bd6fb06dd87bf4fc16896b6f9e0614c66a87
itawareness <ashu.ram@oragetechnologies.com>
2024-11-12 11:52:27 +0530

    commit changes in frontend

--- src/app/client/client-list/client-list.component.ts
+++ src/app/client/client-list/client-list.component.ts
@@ -1,5 +1,3 @@
-// src/app/features/client/client-list/client-list.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../client.service';
 import { Client } from '../models/client.model';
@@ -10,38 +8,127 @@ import { Client } from '../models/client.model';
   styleUrls: ['./client-list.component.css'],
 })
 export class ClientListComponent implements OnInit {
-  clients: Client[] = [];
+  clients: Client[] = [];           // All clients fetched from the server
+  currentPage: number = 0;         // Current page index
+  totalClients: number = 0;        // Total number of clients available from the server
+  totalPages: number = 0;          // Total pages for pagination
+  pageSize: number = 5;            // Number of clients per page
+  searchQuery: string = '';        // Search query for filtering clients
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
-    this.loadClients();
+    this.loadClients(); // Load initial clients when component is initialized
   }
 
-
-  getClients(): void {
-    this.clientService.getClients().subscribe((data: Client[]) => {
-      this.clients = data; // Assign the fetched data to clients
-    });
-  }
-
-  editClient(clientId: number): void {
-    console.log(`Edit client with ID: ${clientId}`);
-    // Here you can navigate to the edit client page or open a modal
-    // For example: this.router.navigate(['/clients/edit', clientId]);
-  }
-
-  // deleteClient(clientId: number): void {
-  //   console.log(`Delete client with ID: ${clientId}`);
-  //   // Call the delete method from your service
-  //   this.clientService.deleteClient(clientId).subscribe(() => {
-  //     this.getClients(); // Refresh the client list after deletion
-  //   });
-  // }
-
+  // Load clients from the API (with pagination)
   loadClients(): void {
-    this.clientService.getClients().subscribe((data) => {
-      this.clients = data;
+    this.clientService.getClients(this.currentPage, this.pageSize, this.searchQuery).subscribe((response) => {
+      this.clients = response.content; // Set the current page clients
+      this.totalClients = response.totalElements; // Set the total number of clients
+      this.totalPages = response.totalPages; // Set the total number of pages
     });
+  }
+
+  // Go to the next page
+  nextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.loadClients(); // Reload the clients for the next page
+    }
+  }
+
+  // Go to the previous page
+  prevPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.loadClients(); // Reload the clients for the previous page
+    }
+  }
+
+  // Go to a specific page
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.loadClients(); // Reload the clients for the specific page
+  }
+
+  // Filter clients based on the search query
+  filterClients(): void {
+    this.currentPage = 0; // Reset to the first page when performing a new search
+    this.loadClients(); // Reload the clients with the search query applied
+  }
+
+  // Getter to get the currently paginated clients
+  get paginatedClients(): Client[] {
+    const start = this.currentPage * this.pageSize;
+    const end = start + this.pageSize;
+    return this.clients.slice(start, end); // Slice the filtered clients array for the current page
   }
 }
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { ClientService } from '../client.service';
+// import { Client } from '../models/client.model';
+
+// @Component({
+//   selector: 'app-client-list',
+//   templateUrl: './client-list.component.html',
+//   styleUrls: ['./client-list.component.css'],
+// })
+// export class ClientListComponent implements OnInit {
+//   clients: Client[] = [];
+//   currentPage: number = 0;
+//   totalPages: number = 0;
+//   totalElements: number = 0;
+//   pageSize: number = 5;  // Adjust this value as needed
+//   searchQuery: string = '';  // Store search query
+
+//   constructor(private clientService: ClientService) {}
+
+//   ngOnInit(): void {
+//     this.loadClients(); // Load first page of clients initially
+//   }
+
+//   // Load clients based on pagination and search query
+//   loadClients(): void {
+//     this.clientService.getClients(this.currentPage, this.pageSize, this.searchQuery).subscribe((response) => {
+//       this.clients = response.content;       // Clients for the current page
+//       this.totalPages = response.totalPages; // Total number of pages
+//       this.totalElements = response.totalElements; // Total number of records
+//     });
+//   }
+
+//   // Go to the next page
+//   nextPage(): void {
+//     if (this.currentPage < this.totalPages - 1) {
+//       this.currentPage++;
+//       this.loadClients();
+//     }
+//   }
+
+//   // Go to the previous page
+//   prevPage(): void {
+//     if (this.currentPage > 0) {
+//       this.currentPage--;
+//       this.loadClients();
+//     }
+//   }
+
+//   // Go to a specific page
+//   goToPage(page: number): void {
+//     this.currentPage = page;
+//     this.loadClients();
+//   }
+
+//   // Method to handle search input change
+//   onSearchChange(): void {
+//     this.currentPage = 0;  // Reset to the first page when searching
+//     this.loadClients();  // Reload clients with the search query
+//   }
+// }
--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -1,25 +1,71 @@
-// src/app/features/client/client.service.ts
+// // src/app/features/client/client.service.ts
+
+// import { Injectable } from '@angular/core';
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Client } from './models/client.model';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ClientService {
+//   private apiUrl = 'http://localhost:8080/clients'; 
+
+//   constructor(private http: HttpClient) {}
+
+//   // getClients(): Observable<Client[]> {
+//   //   return this.http.get<Client[]>(this.apiUrl);
+//   // }
+
+//    // Modified to accept page and size parameters
+//    getClients(page: number = 0, size: number = 10): Observable<any> {
+//     const params = new HttpParams()
+//       .set('page', page.toString())
+//       .set('size', size.toString());
+
+//     return this.http.get<any>(this.apiUrl, { params });
+//   }
+
+//   addClient(client: Client): Observable<Client> {
+//     return this.http.post<Client>(this.apiUrl, client);
+//   }
+
+//   // Additional methods like updateClient, deleteClient can be added here
+// }
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Client } from './models/client.model';
+
+interface PaginatedResponse {
+  content: Client[];        // Paginated client data
+  totalPages: number;       // Total number of pages
+  totalElements: number;    // Total number of records
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService {
-  private apiUrl = 'http://localhost:8080/clients'; 
+  private apiUrl = 'http://localhost:8080/api/clients'; // Your backend API URL
 
   constructor(private http: HttpClient) {}
 
-  getClients(): Observable<Client[]> {
-    return this.http.get<Client[]>(this.apiUrl);
+   // Method to get clients with pagination and search query
+   getClients(page: number, size: number, searchQuery: string = ''): Observable<PaginatedResponse> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString())
+      .set('search', searchQuery); // Pass empty string if search is not provided
+
+    return this.http.get<PaginatedResponse>(this.apiUrl, { params });
   }
+
+  
+
 
   addClient(client: Client): Observable<Client> {
-    return this.http.post<Client>(this.apiUrl, client);
-  }
-
-  // Additional methods like updateClient, deleteClient can be added here
+         return this.http.post<Client>(this.apiUrl, client);
+      }
 }
